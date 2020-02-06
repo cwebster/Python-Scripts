f9eeb132faa3387e72e787c68f5369b3aa5840bc
Craig Webster <craig.webster@mac.com>
2020-02-06 11:23:55 +0000

    update
    Signed-off-by: Craig Webster <craig.webster@mac.com>

--- potassium-analysis.py
+++ potassium-analysis.py
@@ -9,10 +9,10 @@ Created on Tue Dec 24 11:44:15 2019
 """
 
 import pandas as pd
-import plotly.io as pio
-from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import plotly.io as pio
 import webbrowser
+from plotly.subplots import make_subplots
 
 hgs_file = r"hgs_k.csv"
 
@@ -28,6 +28,9 @@ df_qe['NewK'] = pd.to_numeric(df_qe.K, errors='coerce')
 df_hgs.DTR = pd.to_datetime(df_hgs.DTR, errors='coerce')
 df_qe.DTR = pd.to_datetime(df_qe.DTR, errors='coerce')
 
+df_hgs.DTC = pd.to_datetime(df_hgs.DTC, errors='coerce')
+df_qe.DTC = pd.to_datetime(df_qe.DTC, errors='coerce')
+
 df_hgs.FDRPORT = pd.to_datetime(df_hgs.FDRPORT, errors='coerce')
 df_qe.FDRPORT = pd.to_datetime(df_qe.FDRPORT, errors='coerce')
 
@@ -37,3 +40,8 @@ df_qe.FDR = pd.to_datetime(df_qe.FDR, errors='coerce')
 df_hgs.FDAUTH = pd.to_datetime(df_hgs.FDAUTH, errors='coerce')
 df_qe.FDAUTH = pd.to_datetime(df_qe.FDAUTH, errors='coerce')
 
+df_qe['TravelTime'] = (df_qe.DTR-df_qe.DTC).astype('timedelta64[h]')
+df_hgs['TravelTime'] = (df_hgs.DTR-df_hgs.DTC).astype('timedelta64[h]')
+
+df_qe['TravelTime'] = (df_qe.DTR-df_qe.DTC).astype('timedelta64[h]')
+df_hgs['TravelTime'] = (df_hgs.DTR-df_hgs.DTC).astype('timedelta64[h]')
